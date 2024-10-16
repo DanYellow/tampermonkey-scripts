ffce98e930ae0bd574f8bebcd1411191de8ec672
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-10-16 23:34:08 +0200

    feat: Update btn' status on drag-n-drop

--- scodoc/src/drag-and-drop.js
+++ scodoc/src/drag-and-drop.js
@@ -5,17 +5,20 @@ const dragAndDropArea = document.querySelector('[data-drag-n-drop-area]');
 ['dragend', 'dragleave'].forEach(event => {
     dragAndDropArea.addEventListener(event, e => {
         e.preventDefault();
+        e.currentTarget.querySelector(".tp-upload-btn").classList.remove("over");
         e.currentTarget.classList.remove("over");
     });
 });
 
 dragAndDropArea.addEventListener('dragover', e => {
     e.preventDefault();
+    e.currentTarget.querySelector(".tp-upload-btn").classList.add("over");
     e.currentTarget.classList.add("over");
 });
 
 dragAndDropArea.addEventListener('drop', e => {
     e.preventDefault();
+    e.currentTarget.querySelector(".tp-upload-btn").classList.remove("over");
     e.currentTarget.classList.remove("over");
     if (e.dataTransfer.items) {
         [...e.dataTransfer.items].forEach((file, i) => {
